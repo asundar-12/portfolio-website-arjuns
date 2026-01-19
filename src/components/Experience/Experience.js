fe1e12285c7602bb2574c57c9a88261abc12e3bb
Arjun Sundaram <arjun.sundaram99@gmail.com>
2026-01-19 14:00:01 -0500

    changes to about/experience

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,37 +1,36 @@
-import React,{useContext} from 'react';
-
+import React, { useContext } from 'react';
 import { ThemeContext } from '../../contexts/ThemeContext';
-
 import './Experience.css';
-
 import { experienceData } from '../../data/experienceData'
 import ExperienceCard from './ExperienceCard';
 
 function Experience() {
-
     const { theme } = useContext(ThemeContext);
     return (
-        <div className="experience" id="experience" style={{backgroundColor: theme.secondary}}> 
-             <div className="experience-body">
-                 <div className="experience-image">
-                     <img src={theme.expimg} alt="" />
-                 </div>
-                 <div className="experience-description">
-                    <h1 style={{color:theme.primary}}>Experience</h1>
-                    {experienceData.map(exp =>(
-                        <ExperienceCard 
-                            key={exp.id}
-                            id={exp.id}
-                            jobtitle={exp.jobtitle}
-                            description = {exp.description}
-                            tools= {exp.tools}
-                            company={exp.company}
-                            startYear={exp.startYear}
-                            endYear={exp.endYear}
-                            location={exp.location}/>
-                    ))}
-                 </div>
-             </div>
+        <div className="experience" id="experience" style={{ backgroundColor: theme.secondary }}>
+            <div className="experience-body">
+                <div className="experience-image">
+                    <img src={theme.expimg} alt="" />
+                </div>
+                <div className="experience-description">
+                    <h1 style={{ color: theme.primary }}>Experience</h1>
+                    {experienceData.map(company =>
+                        company.roles.map(role => (
+                            <ExperienceCard
+                                key={`${company.id}-${role.jobtitle}-${role.startYear}`}
+                                id={company.id}
+                                jobtitle={role.jobtitle}
+                                description={role.description}
+                                tools={role.tools}
+                                company={company.company}
+                                startYear={role.startYear}
+                                endYear={role.endYear}
+                                location={company.location}
+                            />
+                        ))
+                    )}
+                </div>
+            </div>
         </div>
     )
 }
